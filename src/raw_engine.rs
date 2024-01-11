7ea1aca4a6ddb969cab9dca85086253d5073dd64
Louis Saglio <louis.saglio@dedomainia.com>
2024-01-11 14:48:38 +0100

    Add a raw engine

---
+++ src/raw_engine.rs
@@ -0,0 +1,16 @@
+use std::time::Instant;
+use crate::physics::{apply_force, Population};
+
+pub fn run(particles: Population, iterations: u64) {
+    let mut particles = particles;
+    let start = Instant::now();
+    for _ in 0..iterations {
+        particles = apply_force(&particles)
+    }
+    let duration = start.elapsed();
+    println!("Total time elapsed is: {:?}", duration);
+    println!("Microsec per update: {:?}", duration.as_micros() / iterations as u128);
+    if duration.as_millis() > 0 {
+        println!("UPS: {:?}", (iterations * 1000) / duration.as_millis() as u64);
+    }
+}
