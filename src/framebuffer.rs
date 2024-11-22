2e487a443cc33469bd3eb1786493fa06968ed194
Louis Saglio <louis.saglio@proton.me>
2024-11-23 00:41:42 +0100

    Use signed numbers to represent coordinates in the framebuffer engine

--- src/framebuffer.rs
+++ src/framebuffer.rs
@@ -28,35 +28,35 @@ impl Framebuffer {
         self.mmap.fill(0)
     }
 
-    fn get_buffer_index(x: usize, y: usize) -> usize {
-        (y * SCREEN_WIDTH + x) * BYTES_PER_PIXEL
+    fn get_buffer_index(x: isize, y: isize) -> isize {
+        (y * (SCREEN_WIDTH as isize) + x) * (BYTES_PER_PIXEL as isize)
     }
 
-    pub fn draw_pixel(&mut self, x: usize, y: usize, color: &[u8; BYTES_PER_PIXEL]) {
-        // todo: coordinates need to be signed
+    pub fn draw_pixel(&mut self, x: isize, y: isize, color: &[u8; BYTES_PER_PIXEL]) {
         let anchor_pixel_index = Framebuffer::get_buffer_index(x, y);
-        if anchor_pixel_index + BYTES_PER_PIXEL < FRAMEBUFFER_LENGTH && anchor_pixel_index >= 0 {
+        if anchor_pixel_index >= 0 && anchor_pixel_index + (BYTES_PER_PIXEL as isize) < (FRAMEBUFFER_LENGTH as isize) {
+            let anchor_pixel_index = anchor_pixel_index as usize;
             let pixel_slice = &mut self.mmap[anchor_pixel_index..anchor_pixel_index + BYTES_PER_PIXEL];
             pixel_slice.copy_from_slice(color);
         }
     }
 
-    pub fn draw_square(&mut self, x: usize, y: usize, width: usize, height: usize, color: &[u8; BYTES_PER_PIXEL]) {
-        for i in x..(x + width) {
-            for j in y..(y + height) {
+    pub fn draw_square(&mut self, x: isize, y: isize, width: usize, height: usize, color: &[u8; BYTES_PER_PIXEL]) {
+        for i in x..(x + (width as isize)) {
+            for j in y..(y + (height as isize)) {
                 self.draw_pixel(i, j, color);
             }
         }
     }
 
-    pub fn draw_circle(&mut self, x: usize, y: usize, radius: usize, color: &[u8; BYTES_PER_PIXEL]) {
+    pub fn draw_circle(&mut self, x: isize, y: isize, radius: usize, color: &[u8; BYTES_PER_PIXEL]) {
         let rsqr = (radius * radius) as isize;
         for dx in -(radius as isize)..=(radius as isize) {
             for dy in -(radius as isize)..=(radius as isize) {
                 if dx * dx + dy * dy <= rsqr {
-                    let px = (x as isize + dx) as usize;
-                    let py = (y as isize + dy) as usize;
-                    if px < SCREEN_WIDTH && py < SCREEN_HEIGHT {
+                    let px = x + dx;
+                    let py = y + dy;
+                    if (px as usize) < SCREEN_WIDTH && (py as usize) < SCREEN_HEIGHT {
                         self.draw_pixel(px, py, color);
                     }
                 }
@@ -87,8 +87,8 @@ pub fn sandbox() {
         println!("{:?}", population);
         for particle in population.iter() {
             framebuffer.draw_circle(
-                particle.position[0] as usize,
-                particle.position[1] as usize,
+                particle.position[0] as isize,
+                particle.position[1] as isize,
                 5,
                 &[255, 150, 100, 255],
             );
