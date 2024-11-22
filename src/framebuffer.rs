f3bfe74771239175a117333401c818cec06c04e5
Louis Saglio <louis.saglio@dedomainia.com>
2024-11-22 18:14:49 +0100

    Add primitive abstractions to the framebuffer engine

--- src/framebuffer.rs
+++ src/framebuffer.rs
@@ -1,6 +1,7 @@
 use crate::physics::Population;
 use memmap2::{MmapMut, MmapOptions};
 use std::fs::OpenOptions;
+use std::mem::transmute;
 
 const BYTES_PER_PIXEL: usize = 4;
 const SCREEN_WIDTH: usize = 1920;
@@ -25,22 +26,40 @@ impl Framebuffer {
     pub fn clear(&mut self) {
         self.mmap.fill(0)
     }
+    
+    fn get_buffer_index(x: usize, y: usize) -> usize {
+        (y * SCREEN_WIDTH + x) * BYTES_PER_PIXEL
+    }
 
     pub fn draw_pixel(&mut self, x: usize, y: usize, color: &[u8; BYTES_PER_PIXEL]) {
-        let offset = (y * SCREEN_WIDTH + x) * BYTES_PER_PIXEL;
+        let offset = Framebuffer::get_buffer_index(x, y);
         let pixel_slice = &mut self.mmap[offset..offset + BYTES_PER_PIXEL];
         pixel_slice.copy_from_slice(color);
+    }
+    
+    pub fn draw_square(&mut self, x: usize, y: usize, width: usize, height: usize, color: [u8; BYTES_PER_PIXEL]) {
+        for row_index in 0..height {
+            let anchor_index = Framebuffer::get_buffer_index(x + row_index, y);
+            let slice = &mut self.mmap[anchor_index..((anchor_index + width) * BYTES_PER_PIXEL)];
+            slice.copy_from_slice(&vec![color; width].into_flattened());
+        }
     }
 }
 
 pub fn sandbox(population: Population) {
     let mut framebuffer = Framebuffer::new();
-    framebuffer.clear();
-    for particle in population.iter() {
-        framebuffer.draw_pixel(
-            particle.position[0] as usize,
-            particle.position[1] as usize,
-            &[255, 255, 255, 255],
-        )
+    loop {
+        framebuffer.clear();
+        for particle in population.iter() {
+            println!("{:?}", particle.position);
+            framebuffer.draw_square(
+                (particle.position[0] + 500.0) as usize,
+                (particle.position[1] + 500.0) as usize,
+                20,
+                20,
+                [255, 255, 255, 255],
+            )
+        }
+        break
     }
 }
