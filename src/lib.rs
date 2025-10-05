4d5506f8e1e1498358cf3f8c3e5ea4f13b63b935
Louis Saglio <louis.saglio@proton.me>
2025-10-05 16:35:49 +0200

    Remove the graphical implementation based on piston

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,4 @@
 pub mod framebuffer;
-pub mod graphical_engine;
 pub mod physics;
 pub mod raw_engine;
 pub mod stack_hash_map;
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-use rengine::graphical_engine;
 use rengine::physics::Particle;
 use std::time::Instant;
 
@@ -12,8 +11,6 @@ fn main() {
         None => println!("Build variant not specified"),
         Some(build_variant) => match build_variant {
             "BENCHMARK" => raw_engine::run(Particle::new_random_pop_in_screen(2560, 1440)),
-            "PISTON_DEMO" => graphical_engine::run(Particle::new_random_pop_in_screen(2560, 1440)),
-            "PISTON_TEST" => graphical_engine::run(Particle::new_test_pop()),
             "FRAMEBUFFER" => framebuffer::run(),
             "FRAMEBUFFER_BENCHMARK" => {
                 let start = Instant::now();
